a16b58dbfd0119ad1aa31b8f6bdd832053644be7
Klaasjan Maas <klaasjan.maas@outlook.com>
2023-07-21 16:49:13 +0200

    add git support

--- home/klaasjan/razer.nix
+++ home/klaasjan/razer.nix
@@ -1,3 +1,5 @@
+{ pkgs }:
+
 {
   imports = [
     ../common/global
@@ -8,6 +10,19 @@
     username = "klaasjan";
     homeDirectory = "/home/klaasjan";
     stateVersion = "22.05";
+
+  programs.git = {
+    enable = true;
+    package = pkgs.gitFull;
+    userName = "Klaasjan Maas";
+    userEmail = "klaasjan.maas@outlook.com";
+    aliases = {
+      graph = "log --decorate --oneline --graph";
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+    ignores = [ ".direnv" "result" ];
   };
 
   # Let Home Manager install and manage itself.
