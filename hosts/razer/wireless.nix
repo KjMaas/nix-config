0acf9f4c8bc9a939273b818b09596197ce5264cd
Klaasjan Maas <klaasjan.maas@outlook.com>
2023-07-25 19:42:40 +0200

    update networks

--- hosts/razer/wireless.nix
+++ hosts/razer/wireless.nix
@@ -4,6 +4,7 @@
       hostName = "razer";
 
       networkmanager.enable = false; # Easiest to use and most distros use this by default.
+
       wireless = {
         enable = true; # Enables wireless support via wpa_supplicant.
 
@@ -22,6 +23,22 @@
             psk = "@PSK_MAJOK@";
             priority = 100;
           };
+          "CAMPUS-NUMERIQUE" = {
+            psk = "@PSK_CAMPUS@";
+            priority = 3;
+          };
+          "floki_office_plus" = {
+            psk = "@PSK_FLOKI@";
+            priority = 5;
+          };
+          "floki_office" = {
+            psk = "@PSK_FLOKI@";
+            priority = 4;
+          };
+          "Bbox-9A77B139" = {
+            psk = "@PSK_MONICA@";
+            priority = 10;
+          };
         };
 
         # Imperative networks (manually selectable)
@@ -30,14 +47,11 @@
           enable = true;
           group = "network";
         };
-        extraConfig = ''
-          update_config=1
-          '';
       };
 
   };
 
-  # Ensure group exists
+  # Ensure the network group exists
   users.groups.network = { };
 
   systemd.services.wpa_supplicant.preStart = "touch /etc/wpa_supplicant.conf";
