ff3da0052cc08ad85cf8eb9391c019ba1747cf41
Klaasjan Maas <klaasjan.maas@outlook.com>
2023-08-02 15:56:30 +0200

    add Thunar as default graphical file manager

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -1,6 +1,7 @@
 # These configs are common to all the hosts
+{ pkgs, ... }:
 
-{ inputs, outputs, ... }: {
+{
   imports = [
     ./locale.nix
     ./nix.nix
@@ -17,5 +18,22 @@
 
   # backlight
   programs.light.enable = true;
+
+  # Graphical File Manager
+  programs.thunar = {
+    enable = true;
+    plugins = with pkgs.xfce; [
+      thunar-archive-plugin
+      thunar-volman
+    ];
+  };
+  # Mount, trash, and other functionalities
+  services.gvfs.enable = true;
+  # Thumbnail support for images
+  services.tumbler.enable = true;
+
+  environment.systemPackages = [
+    pkgs.trash-cli # Command line interface to the freedesktop.org trashcan
+  ];
 
 }
