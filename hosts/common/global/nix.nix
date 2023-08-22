9f4e298de41922fe0136770cbad37ef0c9c2d75a
Klaasjan Maas <klaasjan.maas@outlook.com>
2023-08-22 23:03:06 +0200

    automate store optimization and garbage collection

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -1,8 +1,10 @@
 {
   nix = {
-    extraOptions = "experimental-features = nix-command flakes";
 
     settings = {
+
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+
       trusted-users = [ "root" "klaasjan" ];
 
       substituters = [
@@ -12,7 +14,21 @@
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
 
+      # Replace identical files in the store by hard links
+      auto-optimise-store = true;
+
+      # Warn about dirty Git/Mercurial trees.
+      warn-dirty = false;
+
     };
+
+    # Automate Garbage Collection
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+
   };
 }
 
