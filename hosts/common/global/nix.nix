aa04c719e8f1e5f986c143fcf293369a43a915e0
Klaasjan Maas <klaasjan.maas@outlook.com>
2023-11-02 10:01:39 +0100

    Move nix cache config to flake.nix

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -7,13 +7,6 @@
 
       trusted-users = [ "root" "klaasjan" ];
 
-      substituters = [
-        "https://hyprland.cachix.org"
-      ];
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      ];
-
       # Replace identical files in the store by hard links
       auto-optimise-store = true;
 
