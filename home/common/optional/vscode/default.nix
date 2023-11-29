bd55c1ca727046c802b6c7797784c5d0c8ca047f
Klaasjan Maas <klaasjan.maas@outlook.com>
2023-11-29 11:54:53 +0100

    Allow unfree packages to be installed

--- home/common/optional/vscode/default.nix
+++ home/common/optional/vscode/default.nix
@@ -1,13 +1,20 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
+let
+  pkgs-unfree = import inputs.nixpkgs {
+    system = pkgs.system;
+    config.allowUnfree = true;
+  };
+
+in
 {
-
   programs.vscode = {
     # ... no comments please, it's for research
     enable = true;
     
     # ToDo: make the Blender-Development plugin work in VsCodium
     # package = pkgs.vscodium;
+    package = pkgs-unfree.vscode;
 
     extensions = with pkgs.vscode-extensions; [
       arcticicestudio.nord-visual-studio-code
