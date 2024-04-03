4ac81c527c941baafe57612161eb24e44182fefc
Klaasjan Maas <klaasjan.maas@outlook.com>
2024-04-03 11:42:26 +0200

    disable kubernetes for now - try out arion

--- hosts/common/optional/virtualisation/docker.nix
+++ hosts/common/optional/virtualisation/docker.nix
@@ -13,8 +13,9 @@
 
 
   environment.systemPackages = with pkgs; [
+    # arion             # Run docker-compose with help from Nix/NixOS
     docker-compose  # Docker CLI plugin to define and run multi-container applications with Docker 
-    kubernetes      # Production-Grade Container Scheduling and Management
+    # kubernetes      # Production-Grade Container Scheduling and Management
   ];
 
 
