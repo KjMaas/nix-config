3bb43e145ab5ec2748099e9f1a18546cbce6ee42
Klaasjan Maas <klaasjan.maas@outlook.com>
2024-04-14 20:24:26 +0200

    Dirty mixture between hypr-stuff updates and file formatting

--- flake.nix
+++ flake.nix
@@ -2,10 +2,8 @@
   description = "Machine Configurations";
 
   nixConfig = {
-    extra-substituters = [
-      "https://hyprland.cachix.org"
-      "https://devenv.cachix.org"
-    ];
+    extra-substituters =
+      [ "https://hyprland.cachix.org" "https://devenv.cachix.org" ];
     extra-trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
@@ -16,11 +14,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
 
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-    devenv.url = "github:cachix/devenv/main";
-    devenv.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    devenv = {
+      url = "github:cachix/devenv/main";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
 
     hyprland.url = "github:hyprwm/Hyprland";
 
@@ -29,10 +31,17 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    hyprpicker = {
+      url = "github:hyprwm/hyprpicker";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nix-colors.url = "github:misterio77/nix-colors";
-    
-    sops-nix.url = "github:mic92/sops-nix";
-    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     blender-bin.url = "github:edolstra/nix-warez?dir=blender";
   };
@@ -43,85 +52,76 @@
     nixpkgs-unstable,
     home-manager,
     devenv,
-    blender-bin,  # availlable in the flake registry: $ nix registry list
-    ...
-  }@inputs: 
+    blender-bin,
     hyprlock,
+    hyprpicker,
+    ... }@inputs:
 
-  let
-    inherit (self) outputs;
-    lib = nixpkgs.lib;
+    let
+      inherit (self) outputs;
+      lib = nixpkgs.lib;
 
-    forAllSystems = nixpkgs.lib.genAttrs [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
+      forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
 
-  in
-  {
+    in {
 
-    # Devshell for bootstrapping
-    devShells = forAllSystems (system:
+      # Devshell for bootstrapping
+      devShells = forAllSystems (system:
         let pkgs = nixpkgs.legacyPackages.${system};
-        in import ./shell.nix { inherit pkgs; }
-    );
+        in import ./shell.nix { inherit pkgs; });
 
-    nixosConfigurations = {
-      # Main Laptop
-      razer =  lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs outputs; };
-        modules = [ 
-          ./hosts/razer
-          home-manager.nixosModules.home-manager
-          {
-            nixpkgs.overlays = [ 
-              blender-bin.overlays.default
-              devenv.overlays.default
-            ];
-            home-manager = {
-              useGlobalPkgs = true;                   # makes hm use nixos's pkgs value
-              extraSpecialArgs = { inherit inputs; }; # allows access to flake inputs in hm modules
-              users = {
-                klaasjan.imports = [ ./home/klaasjan/razer.nix ];
+      nixosConfigurations = {
+        # Main Laptop
+        razer = lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs outputs; };
+          modules = [
+            ./hosts/razer
+            home-manager.nixosModules.home-manager
+            {
+              nixpkgs.overlays = [
+                blender-bin.overlays.default
+                devenv.overlays.default
                 hyprlock.overlays.default
+                hyprpicker.overlays.default
+              ];
+              home-manager = {
+                useGlobalPkgs = true; # makes hm use nixos's pkgs value
+                extraSpecialArgs = {
+                  inherit inputs;
+                }; # allows access to flake inputs in hm modules
+                users = { klaasjan.imports = [ ./home/klaasjan/razer.nix ]; };
               };
-            };
-          }
-        ];
-      };
-    };
-
-
-    nixopsConfigurations = {
-      default = {
-        inherit nixpkgs;
-        network = {
-          description = "Virtual testing environement";
-          storage.legacy = {
-            databasefile = "~/.nixops/razervm.nixops";
-          };
-        };
-
-        razervm = { ... }: {
-
-          imports =  [
-            ./hosts/razervm
+            }
           ];
-
-          deployment.targetEnv = "virtualbox";
-          deployment.virtualbox.memorySize = 4096;
-          deployment.virtualbox.vcpu = 8;
-
-          deployment.provisionSSHKey = true;
-          deployment.targetUser = "root";
-          deployment.targetHost = "razervm";
-          deployment.targetPort = 22;
-
         };
-
       };
-    };
 
-  };
+      # nixopsConfigurations = {
+      #   default = {
+      #     inherit nixpkgs;
+      #     network = {
+      #       description = "Virtual testing environement";
+      #       storage.legacy = { databasefile = "~/.nixops/razervm.nixops"; };
+      #     };
+
+      #     razervm = { ... }: {
+
+      #       imports = [ ./hosts/razervm ];
+
+      #       deployment.targetEnv = "virtualbox";
+      #       deployment.virtualbox.memorySize = 4096;
+      #       deployment.virtualbox.vcpu = 8;
+
+      #       deployment.provisionSSHKey = true;
+      #       deployment.targetUser = "root";
+      #       deployment.targetHost = "razervm";
+      #       deployment.targetPort = 22;
+
+      #     };
+
+      #   };
+      # };
+
+    };
 }
