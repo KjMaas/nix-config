5329fb147d81aed50b100f9c440f0b58b0438a2c
Klaasjan Maas <klaasjan@majok.dev>
2024-09-04 15:40:08 +0200

    feat: [nvim] increase check for plugin updates to every 6 months

--- home/common/optional/nvim/dotfiles/nvim/lua/config/lazy.lua
+++ home/common/optional/nvim/dotfiles/nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
   checker = {
     enabled = true, -- automatically check for plugin updates
     notify = true,
-    frequency = 60 * 60 * 24 * 7,
+    frequency = 60 * 60 * 24 * 7 * 4 * 6,
   },
 
   ui = {
