668e7ed91d3c22418d6520cbec23b55b2ee41bd6
Klaasjan Maas <klaasjan@majok.dev>
2025-08-10 15:00:29 +0200

    style: [rofi] better UI/UX

--- home/common/optional/wayland/rofi/default.nix
+++ home/common/optional/wayland/rofi/default.nix
@@ -5,16 +5,8 @@ let
   customLib = import ./../../../../../customLib.nix;
   stow_script = customLib.stow_dotfiles_script "common/optional/wayland/rofi";
 
-  # TODO: switch back to mainline nixpkgs once ABI version missmatch is solved for rofi-wayland vs plugins
-  # https://github.com/NixOS/nixpkgs/issues/298539
-  # oldPkgs = import (builtins.fetchTarball {
-  #   url = "https://github.com/NixOS/nixpkgs/releases/tag/23.11";
-  # }) { };
-  pkgs2311 = import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz";
-  }) { };
-
-  rofi = with pkgs2311;
+  rofi =
+    with pkgs;
     rofi-wayland.override {
       plugins = [
         rofi-calc # Do live calculations in rofi!
@@ -22,7 +14,8 @@ let
       ];
     };
 
-in {
+in
+{
 
   home.packages = [ rofi ];
 
@@ -31,14 +24,5 @@ in {
     text = stow_script;
     executable = true;
   };
-
-  # ToDo: nixify the rofi configuration once it's stabilised
-  # programs.rofi = {
-  #   enable = true;
-  #   package = pkgs.rofi-wayland;
-  #   plugins = with pkgs; [ rofi-calc rofi-emoji ];
-  #   location = "bottom";
-  #   theme = ./theme/colors.rasi;
-  # };
 
 }
